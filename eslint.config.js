d9f3bf7df0ac388775a3d73931abba4be4faf026
Alexandre Tonin <xandetonin@gmail.com>
2025-05-23 15:27:49 -0300

    chore: add configuration files for Prettier and ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -2,33 +2,37 @@ const js = require('@eslint/js');
 const react = require('eslint-plugin-react');
 const reactHooks = require('eslint-plugin-react-hooks');
 const expo = require('eslint-config-expo/flat');
+const prettier = required('eslint-config-prettier');
 
 module.exports = [
-	...expo,
-	{
-		ignores: ['dist'],
-	},
-	{
-		files: ['**/*.{js,jsx,ts,tsx}'],
-		settings: {
-			react: { version: '18.3' },
-		},
-		plugins: {
-			react,
-			'react-hooks': reactHooks,
-		},
-		rules: {
-			...js.configs.recommended.rules,
-			...react.configs.recommended.rules,
-			...react.configs['jsx-runtime'].rules,
-			...reactHooks.configs.recommended.rules,
-			'react/jsx-no-target-blank': 'off',
-			quotes: ['error', 'single'],
-			semi: ['error', 'always'],
-			'no-unused-vars': ['error'],
-			'class-methods-use-this': 'off',
-			'no-param-reassign': 'off',
-			'no-console': ['error', { allow: ['warn', 'error'] }],
-		},
-	},
+  ...expo,
+  {
+    ignores: ['dist'],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    settings: {
+      react: { version: '18.3' },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      prettier: require('eslint-plugin-prettier'),
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'no-unused-vars': ['error'],
+      'class-methods-use-this': 'off',
+      'no-param-reassign': 'off',
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+    },
+    prettier,
+  },
 ];
