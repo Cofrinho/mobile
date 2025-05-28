d17e355ff8c4aac0927731bf63ff3382e44e7deb
Alexandre Tonin <xandetonin@gmail.com>
2025-05-28 18:03:32 -0300

    feat: implement login form with validation and error handling

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,12 +1,41 @@
+import ErrorText from '@/components/ErrorText';
 import RoundedButton from '@/components/RoundedButton';
 import RoundedInput from '@/components/RoundedInput';
 import Subtitle from '@/components/Subtitle';
 import Colors from '@/constants/colors';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'expo-router';
+import { useForm } from 'react-hook-form';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { z } from 'zod';
+
+const loginFormSchema = z.object({
+  email: z.string().email('Email inválido'),
+  password: z.string().min(6, 'Senha deve possuir no mínimo 6 caracteres'),
+});
+
+type FormData = z.infer<typeof loginFormSchema>;
 
 export default function Login() {
   const router = useRouter();
+
+  const {
+    register,
+    setValue,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(loginFormSchema),
+  });
+
+  const onSubmit = async (data: FormData) => {
+    // TODO: enviar dados para o back-end validar e fazer a lógica de autenticação baseada na resposta
+    console.log(data);
+  };
+
+  const password = watch('password');
+  const showPasswordError = !!errors.password && password?.length < 6;
 
   return (
     <View style={styles.container}>
@@ -17,29 +46,46 @@ export default function Login() {
 
       <View style={styles.formContainer}>
         <View style={styles.formFieldsContainer}>
-          <RoundedInput
-            placeholder="Email"
-            keyboardType="email-address"
-            autoCapitalize="none"
-            autoCorrect={false}
-            textContentType="emailAddress"
-            autoComplete="email"
-            inputMode="email"
-          />
-          <RoundedInput
-            placeholder="Senha"
-            isPassword
-            keyboardType="default"
-            autoCapitalize="none"
-            autoCorrect={true}
-            textContentType="password"
-            autoComplete="password"
-            inputMode="text"
-          />
+          <View style={styles.inputContainer}>
+            <RoundedInput
+              placeholder="Email"
+              onChangeText={(text) => setValue('email', text)}
+              {...register('email')}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              textContentType="emailAddress"
+              autoComplete="email"
+              inputMode="email"
+            />
+            <View style={styles.formErrorContainer}>
+              {errors.email && <ErrorText text={errors.email.message} />}
+            </View>
+          </View>
+
+          <View style={styles.inputContainer}>
+            <RoundedInput
+              placeholder="Senha"
+              onChangeText={(text) => setValue('password', text)}
+              {...register('password')}
+              isPassword
+              keyboardType="default"
+              autoCapitalize="none"
+              autoCorrect={true}
+              textContentType="password"
+              autoComplete="password"
+              inputMode="text"
+            />
+            <View style={styles.formErrorContainer}>
+              {errors.password && showPasswordError && (
+                <ErrorText text={errors.password?.message} />
+              )}
+            </View>
+          </View>
         </View>
 
         <View style={styles.formButtonContainer}>
-          <RoundedButton text="LOGIN" />
+          <RoundedButton text="LOGIN" onPress={handleSubmit(onSubmit)} />
           <TouchableOpacity onPress={() => router.push('/forgot-password')}>
             <Text style={styles.text}>Esqueceu sua senha?</Text>
           </TouchableOpacity>
@@ -97,5 +143,11 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     gap: 8,
+  },
+  formErrorContainer: {
+    marginLeft: 16,
+  },
+  inputContainer: {
+    gap: 4,
   },
 });
