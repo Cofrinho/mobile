f81e98f4f2dc599b647da05228d451d66843820d
Alexandre Tonin <xandetonin@gmail.com>
2025-06-02 11:00:02 -0300

    feat: add NotificationCard component and integrate with notifications screen

--- app/notifications.tsx
+++ app/notifications.tsx
@@ -1,11 +1,63 @@
 import CircleIconButton from '@/components/CircleIconButton';
+import NotificationCard from '@/components/NotificationCard';
 import Colors from '@/constants/colors';
 import { useRouter } from 'expo-router';
-import { Undo2 } from 'lucide-react-native';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { AlertTriangle, BanknoteArrowUp, PiggyBank, Undo2 } from 'lucide-react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+const notificationsArray = [
+  {
+    id: 'cofrinho',
+    title: 'Recarga cofrinho',
+    value: 49,
+    description: 'Recarga recebida de Nubank',
+    date: '07 abr. • 15:30',
+    icon: <PiggyBank color={Colors.primary} />,
+  },
+  {
+    id: 'group',
+    title: 'Racha churras',
+    value: 75,
+    description: 'Contribua com o churras',
+    date: '07 abr. • 15:30',
+    icon: <BanknoteArrowUp color={Colors.primary} />,
+    group: '1',
+  },
+  {
+    id: 'due',
+    title: 'Lembrete',
+    value: 77,
+    description: 'Conta de luz vence amanhã',
+    date: '07 abr. • 15:30',
+    icon: <AlertTriangle color={Colors.primary} />,
+    group: '4',
+  },
+];
+
+interface notification {
+  id: string;
+  title: string;
+  value: number;
+  description: string;
+  date: string;
+  icon: React.ReactNode;
+  group?: string;
+}
 
 export default function Notifications() {
   const router = useRouter();
+
+  const renderItem = ({ item }: { item: notification }) => (
+    <NotificationCard
+      id={item.id}
+      title={item.title}
+      value={item.value}
+      icon={item.icon}
+      description={item.description}
+      date={item.date}
+      group={item.group}
+    />
+  );
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
@@ -18,6 +70,14 @@ export default function Notifications() {
           />
           <Text style={styles.title}>Notificações</Text>
         </View>
+
+        <FlatList
+          data={notificationsArray}
+          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
+          contentContainerStyle={{ gap: 12, paddingBottom: 16 }}
+          showsVerticalScrollIndicator={false}
+        />
       </View>
     </SafeAreaView>
   );
@@ -33,6 +93,7 @@ const styles = StyleSheet.create({
     position: 'relative',
     height: 48,
     justifyContent: 'center',
+    marginBottom: 12,
   },
   title: {
     position: 'absolute',
